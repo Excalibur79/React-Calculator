56b8388b0474fcdc75d8d3194164942241ef4689
Ankur Saha <ankursahamoto@gmail.com>
2020-08-29 01:43:58 -0800

    Updates

---
+++ precache-manifest.8fb8544b8ce2bd3102842c7473051a42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b75468c49165c43d7cf24ad8ec312801",
+    "url": "/React-Calculator/index.html"
+  },
+  {
+    "revision": "81771198455e075ae913",
+    "url": "/React-Calculator/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "f4ce4e9721ad0f56d0c3",
+    "url": "/React-Calculator/static/css/main.e6762f2a.chunk.css"
+  },
+  {
+    "revision": "81771198455e075ae913",
+    "url": "/React-Calculator/static/js/2.f5ff8b2d.chunk.js"
+  },
+  {
+    "revision": "a8eb76e4c6beb72c22bcc9f3c026930b",
+    "url": "/React-Calculator/static/js/2.f5ff8b2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4ce4e9721ad0f56d0c3",
+    "url": "/React-Calculator/static/js/main.1210c73f.chunk.js"
+  },
+  {
+    "revision": "31f1b382c927167c40c0",
+    "url": "/React-Calculator/static/js/runtime-main.b6e8a5a6.js"
+  }
+]);
